d9c1f6d9a0b2d404a54b55811c90b489eddcdb4e
mkaa <20796385+thomasbad@users.noreply.github.com>
2023-03-24 14:33:26 +0800

    Project first create

---
+++ app/src/main/java/com/mka/filmviewer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mka.filmviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
